737bd1d022997807aa09ce881721c547789b50d7
davilos <daviloscostagg@hotmail.com>
2023-10-11 18:07:32 -0300

    fix the http 403 status code

--- aplicacoes/secao4/escola/cursos/permissions.py
+++ aplicacoes/secao4/escola/cursos/permissions.py
@@ -5,7 +5,9 @@ class IsSuperUser(permissions.BasePermission):
 
     def has_permission(self, request, view):
 
-        if request.method == "DELETE" and request.user.is_superuser:
-            return True
-        return False
+        if request.method == "DELETE":
+            if request.user.is_superuser:
+                return True
+            return False
+        return True
 
